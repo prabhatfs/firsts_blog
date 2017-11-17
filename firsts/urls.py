1480cc597bd56c0b7c9a6e266fc0b3c539bb5217
Admin <prabhat@firstsynch.com>
2017-11-17 15:57:56 +0530

    kishans suggestions

--- firsts/urls.py
+++ firsts/urls.py
@@ -38,7 +38,7 @@ urlpatterns += [
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    url("^$", direct_to_template, {"template": "pages/homepage.html"}, name="home"),
+    url("^$", direct_to_template, {"template": "homepage.html"}, name="home"),
     #url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
--- theme/templatetags/theme_tags.py
+++ theme/templatetags/theme_tags.py
@@ -13,3 +13,19 @@ def get_site_conf():
     Adds the `SiteConfiguration` to the context
     """
     return SiteConfiguration.objects.get_or_create(site_id=current_site_id())[0]
+
+@register.as_tag
+def blog_ranges(l):
+	return range(3,len(l),3)
+
+def blog_lookup(d, key):
+	return d[key]
+
+def blog_slice(d,start):
+	if len(d) > start + 3:
+		return d[start:start+3]
+	else:
+		return d[start:]
+
+register.filter('blog_lookup', blog_lookup)
+register.filter('blog_slice', blog_slice)
